20feaf89adcba4421da3b6fe9035e6b579b69b29
Raihan115219 <aburaihan115219@gmail.com>
2023-05-04 14:46:44 +0600

    file name changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "../src/router.js";
+import { router } from "./router.js";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router}>
